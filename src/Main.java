9299cb400df50460c83309044c060aa03fb5b327
Admin <k.i.r.s@bk.ru>
2022-10-17 20:03:57 +0300

    Сделано ДЗ 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!)))");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+            var friend = 19;
+            System.out.println(friend);
+            friend = friend +2;
+             System.out.println(friend);
+            friend = friend / 7;
+            System.out.println(friend);
+                var frog = 3.5;
+                System.out.println(frog);
+                frog = frog * 10;
+                System.out.println(frog);
+                frog = frog / 3.5;
+                System.out.println(frog);
+                frog = frog + 4;
+                System.out.println(frog);
+                var firstBoxer = 78.2;
+                var secondBoxer = 82.7;
+                var Boxer = firstBoxer + secondBoxer;
+                System.out.println("общий вес боксеров " + Boxer + "кг.");
+                var differenceBoxer = secondBoxer - firstBoxer;
+                System.out.println("разница в весе боксеров " + differenceBoxer + "кг.");
+                differenceBoxer = secondBoxer % firstBoxer;
+                System.out.println("разница в весе боксеров " + differenceBoxer + "кг.");
+                    var employee = 640 / 8;
+                     System.out.println("Всего работников в компании – " + employee + " человек" );
+                     var newEmployee = 94;
+                     var work = newEmployee*8;
+                     System.out.println("Если в компании работает " + newEmployee + " человек, то всего " + work + "часов работы может быть поделено между сотрудниками" );
     }
 }
